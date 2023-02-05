62d0f8a7f51a539549a9d41d70b786440cd21b5b
Asif <67794014+nortaip@users.noreply.github.com>
2023-02-05 12:54:12 +0400

    6756

--- src/components/LotosA.jsx
+++ src/components/LotosA.jsx
@@ -3,29 +3,35 @@ import styles, { layout } from "../style";
 
 const LotosA = () => (
     <section id="products" className={`${layout.sectionReverse} `}>
+
         <div className={layout.sectionImgReverse}>
-            <img src={Lotos3} alt="billing" className="w-[100%] h-[100%]  relative z-[5] rounded" />
+            <a target={"_blank"} href="https://www.jiedemedical.com/product/110.html">
+                <img src={Lotos3} alt="billing" className="w-[100%] h-[100%]  relative z-[5] rounded" />
+            </a>
         </div>
         <div className={layout.sectionInfo}>
             <h2 className={styles.heading2}>
-                Lotos-A
+                <a target={"_blank"} href="https://www.jiedemedical.com/product/110.html">
+                    Lotos-A</a>
             </h2>
             <p className={`${styles.paragraph} mt-5 text-black`}>
-                90x 200, 140x 200,160x200, 180x200 sm ölcüdə  ortopedik yatak, yüksek sertlik
-                1- Doşek 25 cm yüksəkliktə
-                2-900 gr sert keçe
-                3-22 danste 6x16 cm yan destek sünger takozlu,yay karkas sistemi
-                4-20 danste 9 adet iç 5x15 süngər takozlu
-                5-22 danste list sünger üst və altta
-                6-200 gr. hörgülü pamuk örnek kumaş, antibakterial, su keçirmeyen, kenarları zəncirli( asan çıxarıb yiyulması üçün), ləkə asan təmizlənən
-                7-2.1 cm kapitone süngeri
-                8-Özəl lotus deseni
-                9-Oval hava komsülleri
-                10-Yazılı yan kulplar
+                <a target={"_blank"} href="https://www.jiedemedical.com/product/110.html">
+                    90x 200, 140x 200,160x200, 180x200 sm ölcüdə  ortopedik yatak, yüksek sertlik
+                    1- Doşek 25 cm yüksəkliktə
+                    2-900 gr sert keçe
+                    3-22 danste 6x16 cm yan destek sünger takozlu,yay karkas sistemi
+                    4-20 danste 9 adet iç 5x15 süngər takozlu
+                    5-22 danste list sünger üst və altta
+                    6-200 gr. hörgülü pamuk örnek kumaş, antibakterial, su keçirmeyen, kenarları zəncirli( asan çıxarıb yiyulması üçün), ləkə asan təmizlənən
+                    7-2.1 cm kapitone süngeri
+                    8-Özəl lotus deseni
+                    9-Oval hava komsülleri
+                    10-Yazılı yan kulplar
+                </a>
             </p>
         </div>
 
-    </section>
+    </section >
 );
 
 export default LotosA;
